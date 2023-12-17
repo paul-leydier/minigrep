f3c33ff7a821d0b7fbabaa1da23f2f9cb80ddfe2
Paul Leydier <paul.leydier@compass-ft.com>
2023-12-17 09:26:38 -0500

    ♻️ refactor configuration parsing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::{env, fs};
 
-fn main() {
+fn main() -> Result<(), &'static str>{
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::build(&args)?;
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
     let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
     println!("With text:\n{contents}");
+    Ok(())
 }
 
 struct Config {
@@ -15,10 +16,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Config{query: query, file_path: file_path}
+        Ok(Config{query: query, file_path: file_path})
     }
 }
 
