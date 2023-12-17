5cd17ba50e65f4d6adfd8cdd157b9fe0081d01b8
Paul Leydier <paul.leydier@compass-ft.com>
2023-12-17 15:11:53 -0500

    ✨ search function

--- src/lib.rs
+++ src/lib.rs
@@ -24,3 +24,47 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
     Ok(())
 }
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, content));
+    }
+
+    #[test]
+    fn two_results() {
+        let query = "duct";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Better than duct tape!";
+        assert_eq!(
+            vec!["safe, fast, productive.", "Better than duct tape!"],
+            search(query, content)
+        );
+    }
+
+    #[test]
+    fn zero_result() {
+        let query = "duct";
+        let content = "\
+Rust:
+Pick three.";
+        assert_eq!(Vec::<&str>::new(), search(query, content));
+    }
+}
