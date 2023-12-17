8894ba7656c4cbbc30e72691eb681824ff4804e1
Paul Leydier <paul.leydier@compass-ft.com>
2023-12-17 14:53:26 -0500

    🔇remove now useless logs

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ use minigrep::Config;
 fn main() -> Result<(), Box<dyn Error>>{
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args)?;
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
     minigrep::run(config)?;
     Ok(())
 }
