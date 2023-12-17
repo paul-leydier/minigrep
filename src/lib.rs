7b286b28ffb463739a7d07e77c5759f89a36e2d8
Paul Leydier <paul.leydier@compass-ft.com>
2023-12-17 15:29:15 -0500

    ✨ case insensitive search

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use std::error::Error;
-use std::fs;
+use std::{env, fs};
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -13,17 +14,24 @@ impl Config {
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
         Ok(Config {
             query: query,
             file_path: file_path,
+            ignore_case: ignore_case,
         })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-    for found in search(&config.query, &contents) {
-        println!("{}", found);
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+    for result in results {
+        println!("{}", result);
     }
     Ok(())
 }
@@ -32,6 +40,13 @@ fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     contents
         .lines()
         .filter(|line| line.contains(query))
+        .collect()
+}
+
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
         .collect()
 }
 
@@ -70,5 +85,25 @@ Better than duct tape!";
 Rust:
 Pick three.";
         assert_eq!(Vec::<&str>::new(), search(query, content));
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "rUst";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(Vec::<&str>::new(), search(query, content));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUst";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["Rust:"], search_case_insensitive(query, content));
     }
 }
