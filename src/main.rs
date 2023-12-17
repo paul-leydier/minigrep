1161f4ef11950ec681cde51d8de9fa14b80da89e
Paul Leydier <paul.leydier@compass-ft.com>
2023-12-17 09:17:31 -0500

    ✨ parse arguments into a Config struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    println!("With text:\n{contents}");
 }
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config{query: query, file_path: file_path}
+    }
+}
+
