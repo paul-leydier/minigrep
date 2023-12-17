094c5a4d0228e7bdb685221ec048b9b6423539ba
Paul Leydier <paul.leydier@compass-ft.com>
2023-12-17 09:39:07 -0500

    ♻️ extract most logic to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,12 @@
-use std::{env, fs};
+use std::env;
 use std::error::Error;
+use minigrep::Config;
 
 fn main() -> Result<(), Box<dyn Error>>{
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args)?;
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
-    run(config)?;
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Config{query: query, file_path: file_path})
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("With text:\n{contents}");
+    minigrep::run(config)?;
     Ok(())
 }
